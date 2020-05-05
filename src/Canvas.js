d6dd2d1737580844bb9a71b5db81fcf0c0a539df
ShuyunLiuRun <lsy2601162193@gmail.com>
2020-05-05 14:09:31 +1200

    integrated

---
+++ src/Canvas.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Canvas = ({children}) =>
+    <svg height="200" width="500">
+        {children}
+    </svg>
+
+export default Canvas
--- src/timeLine.js
+++ src/timeLine.js
@@ -8,6 +8,8 @@
 import * as d3 from 'd3'
 import React from 'react'
 import {render} from 'react-dom'
+import Canvas from './Canvas'
+import TimelineDot from './TimelineDot'
 
 const historicDatesForSkiing = [{
     year:1879,
@@ -47,50 +49,59 @@ class Timeline extends React.Component{
         const range = [50,450]
         super({data})
         this.state = {data,times,range}
-        this.targetRef = React.createRef()
+        // this.targetRef = React.createRef()
+        this.scale=d3.scaleTime(times,range)
     }
-    componentDidMount(){
-        let group
-        const {data,times,range} = this.state
-        const target = this.targetRef.current
-        //创建标尺。标尺是根据传递的时间段和像素区间到D3的time。scale函数而创建的
-        //scale函数是用来在可视化应用中为每个位于1879年和1964年之间的大事日期获取x坐标位置的。
-        //const scale = d3.time.scale.domin(times).range(range)
-        const scale = d3.scaleTime(times,range)
+    // componentDidMount(){
+    //     let group
+    //     const {data,times,range} = this.state
+    //     const target = this.targetRef.current
+    //     //创建标尺。标尺是根据传递的时间段和像素区间到D3的time。scale函数而创建的
+    //     //scale函数是用来在可视化应用中为每个位于1879年和1964年之间的大事日期获取x坐标位置的。
+    //     //const scale = d3.time.scale.domin(times).range(range)
+    //     const scale = d3.scaleTime(times,range)
 
-        d3.select(target)
-            .append('svg')
-            .attr('height',200)
-            .attr('width',500)
+    //     d3.select(target)
+    //         .append('svg')
+    //         .attr('height',200)
+    //         .attr('width',500)
 
-        group = d3.select(target.children[0])
-            .selectAll('g')
-            .data(data)
-            .enter()
-            .append('g')
-            .attr(
-                'transform',
-                (d,i) => 'translate(' + scale(d.year) + ',0)'
-            )
+    //     group = d3.select(target.children[0])
+    //         .selectAll('g')
+    //         .data(data)
+    //         .enter()
+    //         .append('g')
+    //         .attr(
+    //             'transform',
+    //             (d,i) => 'translate(' + scale(d.year) + ',0)'
+    //         )
 
-        group.append('circle')
-            .attr('cy',160)
-            .attr('r',5)
-            .style('fill','blue')
+    //     group.append('circle')
+    //         .attr('cy',160)
+    //         .attr('r',5)
+    //         .style('fill','blue')
 
-        group.append('text')
-            .text(d=> d.year + " - " + d.event)
-            .style('font-size',10)
-            .attr('y',115)
-            .attr('x',-95)
-            .attr('transform', 'rotate(-45)')
-    }
+    //     group.append('text')
+    //         .text(d=> d.year + " - " + d.event)
+    //         .style('font-size',10)
+    //         .attr('y',115)
+    //         .attr('x',-95)
+    //         .attr('transform', 'rotate(-45)')
+    // }
 
     render(){
+        const {data} = this.state
+        const {scale} = this
         return(
             <div className="timeline">
                 <h1>{this.props.name} Timeline</h1>
-                <div ref={this.targetRef}></div>
+                {/* <div ref={this.targetRef}></div> */}
+                <Canvas>
+                    {data.map((d,i) =>
+                    <TimelineDot position={scale(d.year)}
+                                    txt={`${d.year} - ${d.event}`}/>
+                    )}
+                </Canvas>
             </div>
         )
     }
